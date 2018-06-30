9e8636acae4f60ef7ba468645fe131e2890ac4e5
roxma <roxma@qq.com>
2018-06-30 10:54:43 +0800

    fix #15 wrap nvim connect in try-finally-close
    move rtp sync and channel started into setup handler

--- pythonx/yarp.py
+++ pythonx/yarp.py
@@ -9,36 +9,14 @@ serveraddr = sys.argv[1]
 yarpid = int(sys.argv[2])
 module = sys.argv[3]
 module_obj = None
+nvim = None
 
 environ['NVIM_YARP_MODULE'] = module
 
 setup_logging(module)
 
-# create another connection to avoid synchronization issue?
-if len(serveraddr.split(':')) == 2:
-    serveraddr, port = serveraddr.split(':')
-    port = int(port)
-    nvim = attach('tcp', address=serveraddr, port=port)
-else:
-    nvim = attach('socket', path=serveraddr)
-
-sys.modules['vim'] = nvim
-sys.modules['nvim'] = nvim
-
-paths = nvim.eval(r'globpath(&rtp,"pythonx",1) . "\n" .'
-                  r' globpath(&rtp,"rplugin/python3",1)')
-for path in paths.split("\n"):
-    if not path:
-        continue
-    if path not in sys.path:
-        sys.path.append(path)
-
-nvim.call('yarp#core#channel_started', yarpid, nvim.channel_id)
-
-module_obj = importlib.import_module(module)
-
-
 def on_request(method, args):
+    global module_obj
     if hasattr(module_obj, method):
         return getattr(module_obj, method)(*args)
     else:
@@ -46,6 +24,7 @@ def on_request(method, args):
 
 
 def on_notification(method, args):
+    global module_obj
     if hasattr(module_obj, method):
         getattr(module_obj, method)(*args)
     else:
@@ -54,10 +33,33 @@ def on_notification(method, args):
 
 
 def on_setup():
-    pass
+    global nvim
+    paths = nvim.eval(r'globpath(&rtp,"pythonx",1) . "\n" .'
+                      r' globpath(&rtp,"rplugin/python3",1)')
+    for path in paths.split("\n"):
+        if not path:
+            continue
+        if path not in sys.path:
+            sys.path.append(path)
+
+    nvim.call('yarp#core#channel_started', yarpid, nvim.channel_id)
+
+    global module_obj
+    module_obj = importlib.import_module(module)
 
 
 try:
+    # create another connection to avoid synchronization issue?
+    if len(serveraddr.split(':')) == 2:
+        serveraddr, port = serveraddr.split(':')
+        port = int(port)
+        nvim = attach('tcp', address=serveraddr, port=port)
+    else:
+        nvim = attach('socket', path=serveraddr)
+
+    sys.modules['vim'] = nvim
+    sys.modules['nvim'] = nvim
+
     nvim.run_loop(on_request, on_notification, on_setup)
 finally:
     nvim.close()
